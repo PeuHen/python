1012c893243bed53bc22aa2f89882908cabe196e
PeuHen <165822649+PeuHen@users.noreply.github.com>
2024-04-24 19:39:22 -0300

    Add files via upload

---
+++ aula/index.py
@@ -0,0 +1,77 @@
+from flask import Flask, redirect,render_template, request
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# a linha abaixo cria uma chave de segurança
+app.secret_key = 'aprendendodoiniciocomdaniel'
+
+#** a linha abaixo configura o acesso ao banco de dados
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+        SGBD = 'mysql+mysqlconnector', 
+        usuario = 'aluno',
+        senha = 'toor',
+        servidor = 'localhost',
+        database = 'loja'
+    )
+
+# a linha abaixo cria uma instancia com a SQLALCHEMY
+db = SQLAlchemy(app)
+
+class Produto(db.Model):
+      id_produto = db.Column(db.Integer, primary_key=True,autoincrement=True)
+      nome_produto = db.Column(db.String(50), nullable=False)
+      marca_produto = db.Column(db.String(40), nullable=False)
+      preco_produto = db.Column(db.Float, nullable=False)
+
+      def __repr__(self):
+            return '<Name %r>' % self.name
+
+#Linha acima configura o acesso ao usuário
+
+@app.route('/inicio')
+def ola():
+        return'<h1> Iniciando flask</h1>'
+
+
+@app.route('/lista')
+def lista():
+
+    produtos_cadastrados = Produto.query.order_by(Produto.id_produto)
+       
+    return render_template('lista.html', descricao = "aqui estão os seus produtos cadastrados",
+                               lista_prod = produtos_cadastrados)
+
+@app.route('/cadastrar' )
+def cadastrar_produto():
+    return render_template('cadastrar.html')
+
+@app.route('/adiciona',methods=['POST',] )
+def adicionar_produto():
+
+    # as variaveis abaixo recebem as informações digitadas pelo user.
+    nome_prod = request.form ['txtNome']
+
+    marca_prod = request.form ['txtMarca']
+
+    #a linha abaixo altera o ',' por '.'
+
+    preco_prod = request.form['txtPreco'].replace(',','.') #replace vai alterar algo ('ISSO',"PORISSO")
+    #linha abaixo converte o valor digitado pelo user.
+    preco_prod = float(preco_prod)
+
+    produto_adicionado = Produto(nome_produto= nome_prod, marca_produto = marca_prod, preco_produto=preco_prod)
+    #linha abaixo é feitp um preparo pro envio ao banco de dados
+
+    db.session.add(produto_adicionado)
+    #na linha de baixo envia para o banco de dados
+    db.session.commit()
+    
+
+    return redirect('/lista')
+
+app.run()
+
+# pip install flask
